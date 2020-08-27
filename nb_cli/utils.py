8c6e2cdebdac49728f3c9937bc1573e451c07feb
yanyongyu <yanyongyu_1@126.com>
2020-08-27 23:19:57 +0800

    :sparkles: add command line and interactive support

---
+++ nb_cli/utils.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import click
+from PyInquirer import style_from_dict, Token
+
+list_style = style_from_dict({
+    Token.Separator: "#6C6C6C",
+    Token.QuestionMark: "#FF9D00 bold",
+    Token.Selected: "#5F819D",
+    Token.Pointer: "#FF9D00 bold",
+    Token.Instruction: "",
+    Token.Answer: "#5F819D bold",
+    Token.Question: "",
+})
+
+
+class ClickAliasedGroup(click.Group):
+
+    def __init__(self, *args, **kwargs):
+        super(ClickAliasedGroup, self).__init__(*args, **kwargs)
+        self._commands = {}
+        self._aliases = {}
+
+    def command(self, *args, **kwargs):
+        aliases = kwargs.pop("aliases", [])
+        decorator = super(ClickAliasedGroup, self).command(*args, **kwargs)
+        if not aliases:
+            return decorator
+
+        def _decorator(f):
+            cmd = decorator(f)
+            if aliases:
+                self._commands[cmd.name] = aliases
+                for alias in aliases:
+                    self._aliases[alias] = cmd.name
+            return cmd
+
+        return _decorator
+
+    def group(self, *args, **kwargs):
+        aliases = kwargs.pop("aliases", [])
+        decorator = super(ClickAliasedGroup, self).group(*args, **kwargs)
+        if not aliases:
+            return decorator
+
+        def _decorator(f):
+            cmd = decorator(f)
+            if aliases:
+                self._commands[cmd.name] = aliases
+                for alias in aliases:
+                    self._aliases[alias] = cmd.name
+            return cmd
+
+        return _decorator
+
+    def resolve_alias(self, cmd_name):
+        if cmd_name in self._aliases:
+            return self._aliases[cmd_name]
+        return cmd_name
+
+    def get_command(self, ctx, cmd_name):
+        cmd_name = self.resolve_alias(cmd_name)
+        command = super(ClickAliasedGroup, self).get_command(ctx, cmd_name)
+        if command:
+            return command
+
+    def format_commands(self, ctx, formatter):
+        rows = []
+
+        sub_commands = self.list_commands(ctx)
+
+        max_len = max(len(cmd) for cmd in sub_commands)
+        limit = formatter.width - 6 - max_len
+
+        for sub_command in sub_commands:
+            cmd = self.get_command(ctx, sub_command)
+            if cmd is None:
+                continue
+            if hasattr(cmd, "hidden") and cmd.hidden:
+                continue
+            if sub_command in self._commands:
+                aliases = ",".join(sorted(self._commands[sub_command]))
+                sub_command = "{0} ({1})".format(sub_command, aliases)
+            cmd_help = cmd.get_short_help_str(limit)
+            rows.append((sub_command, cmd_help))
+
+        if rows:
+            with formatter.section("Commands"):
+                formatter.write_dl(rows)
