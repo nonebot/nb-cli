3776ffb1630f4339b1a9aa2a44daa28730426362
yanyongyu <yanyongyu_1@126.com>
2020-09-12 14:28:57 +0800

    :alembic: add env file for template

--- nb_cli/{{cookiecutter.project_slug}}/bot.py
+++ nb_cli/{{cookiecutter.project_slug}}/bot.py
@@ -3,12 +3,27 @@
 
 import nonebot
 
+# Custom your logger
+# 
+# from nonebot.log import logger, default_format
+# logger.add("error.log",
+#            rotation="00:00",
+#            diagnose=False,
+#            level="ERROR",
+#            format=default_format)
 
-nonebot.init(){% if cookiecutter.load_builtin == "True" %}
+# You can pass some keyword args config to init function
+nonebot.init()
+app = nonebot.get_asgi()
+{% if cookiecutter.load_builtin == "True" %}
 nonebot.load_builtin_plugins(){% endif %}
 nonebot.load_plugins("{{ cookiecutter.source_dir }}/plugins")
 
-app = nonebot.get_asgi()
+# Modify some config / config depends on loaded configs
+# 
+# config = nonebot.get_driver().config
+# do something...
+
 
 if __name__ == "__main__":
     nonebot.run(app="bot:app")
