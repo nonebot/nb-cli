3088dd4512a3f5aeeb5483cca36748cd34f4e1af
jigsaw <j1g5aw@foxmail.com>
2022-01-17 20:18:14 +0800

    :sparkles: Check compatoble for package

--- nb_cli/handlers/_pip.py
+++ nb_cli/handlers/_pip.py
@@ -1,5 +1,6 @@
 from typing import List, Callable, Optional
 
+import httpx
 import pkg_resources
 
 working_set = pkg_resources.working_set
@@ -8,7 +9,32 @@ entry_point = next(working_set.iter_entry_points("console_scripts", "pip"))
 pipmain: Callable[[Optional[List[str]]], int] = entry_point.load()
 
 
+def _calling_pip_install(package: str) -> str:
+    releases = list(
+        httpx.get(f"https://pypi.org/pypi/{package}/json")
+        .json()["releases"]
+        .keys()
+    )
+    releases.reverse()
+    for release in releases:
+        requires_dist = releases = httpx.get(
+            f"https://pypi.org/pypi/{package}/{release}/json"
+        ).json()["info"]["requires_dist"]
+
+        is_compatible = None
+        for dist in requires_dist:
+            if "nonebot2" in dist:
+                is_compatible = True
+                if ">=2.0.0b1" in dist or ">=2.0.0-beta.1" in dist:
+                    is_compatible = False
+                    break
+        if is_compatible is True:
+            return package + "==" + release
+    raise Exception
+
+
 def _call_pip_install(package: str, index: Optional[str] = None) -> int:
+    package = _calling_pip_install(package)
     if index:
         return pipmain(["install", "-i", index, package])
     else:
