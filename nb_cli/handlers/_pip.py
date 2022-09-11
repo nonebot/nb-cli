8ad5d812fb06b1b36966f0d6f8bc1f6e05dda737
StarHeartHunt <starheart233@gmail.com>
2022-09-11 12:37:11 +0800

    :bug: improve cross-platform compatibility

--- nb_cli/handlers/_pip.py
+++ nb_cli/handlers/_pip.py
@@ -3,22 +3,37 @@ from typing import Optional
 from nb_cli.utils import run_script
 
 
-def _call_pip_install(package: str, index: Optional[str] = None) -> int:
+def _call_pip_install(
+    package: str, index: Optional[str] = None, python_path: str = "python"
+) -> int:
     if index:
-        cmd = ["pip", "install", package]
+        cmd = [python_path, "-m", "pip", "install", package]
     else:
-        cmd = ["pip", "install", "-i", index, package]
+        cmd = [python_path, "-m", "pip", "install", "-i", index, package]
     return run_script(cmd, call=True)
 
 
-def _call_pip_update(package: str, index: Optional[str] = None) -> int:
+def _call_pip_update(
+    package: str, index: Optional[str] = None, python_path: str = "python"
+) -> int:
     if index:
-        cmd = ["pip", "install", "--upgrade", "-i", index, package]
+        cmd = [
+            python_path,
+            "-m",
+            "pip",
+            "install",
+            "--upgrade",
+            "-i",
+            index,
+            package,
+        ]
     else:
-        cmd = ["pip", "install", "--upgrade", package]
+        cmd = [python_path, "-m", "pip", "install", "--upgrade", package]
 
     return run_script(cmd, call=True)
 
 
-def _call_pip_uninstall(package: str) -> int:
-    return run_script(["pip", "uninstall", package], call=True)
+def _call_pip_uninstall(package: str, python_path: str = "python") -> int:
+    return run_script(
+        [python_path, "-m", "pip", "uninstall", package], call=True
+    )
