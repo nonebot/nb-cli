2bbeaa5eac8bc06db67dba7cf41e51fe110d9843
eya46 <644310932@qq.com>
2023-07-08 14:21:30 +0800

    fastgit.org改为fgit.ml drivers和adapters改为注入类型

--- nb_cli/handlers/store.py
+++ nb_cli/handlers/store.py
@@ -52,7 +52,7 @@ else:
         exceptions: List[Exception] = []
         urls = [
             f"https://nonebot.dev/{module_name}.json",
-            f"https://raw.fastgit.org/nonebot/nonebot2/master/website/static/{module_name}.json",
+            f"https://raw.fgit.ml/nonebot/nonebot2/master/website/static/{module_name}.json",
             f"https://cdn.jsdelivr.net/gh/nonebot/nonebot2@master/website/static/{module_name}.json",
             f"https://cdn.staticaly.com/gh/nonebot/nonebot2@master/website/static/{module_name}.json",
             f"https://jsd.cdn.zzko.cn/gh/nonebot/nonebot2@master/website/static/{module_name}.json",
